cb5306628622ce16e9124ff4ba5e8c3fcacfac2d
Valerii Pozhydaiev <p_vf@i.ua>
2023-07-07 18:24:44 +0300

    connect to mongoose

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT;
+const DB_HOST = process.env.DB_HOST;
 
 const app = require("./app");
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("DB Connected!");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
